454ca7ac5b6ddbed6d3591af677bdf92d82a91b5
Francisco Blanco <blancofrancisco34@gmail.com>
2026-05-12 16:17:23 -0500

    update fix

--- update.php
+++ update.php
@@ -21,21 +21,31 @@ if (!function_exists("github_updater_plugin_wordpress_v1")) {
                     $plugin_file = $plugin_slug . '/' . $plugin_file_php;
                     $github_api_url = 'https://api.github.com/repos/' . $config['path_repository'] . '/releases/latest';
 
+                    // ⚠️ Asegúrate de almacenar el token de manera segura
+                    $github_token = join('', $config['token_array_split']);
 
-                    // Llamada a la API de GitHub
-                    $response = wp_remote_get($github_api_url, [
-                        'headers' => [
-                            'User-Agent' => 'WordPress-GitHub-Updater',
-                            'Accept' => 'application/vnd.github+json',
-                        ]
-                    ]);
+                    $cache_key = 'github_updater_' . md5($plugin_file);
+                    $release = get_transient($cache_key);
+                    if (!$release) {
+                        // Llamada a la API de GitHub
+                        $response = wp_remote_get($github_api_url, [
+                            'headers' => [
+                                'User-Agent'    => 'WordPress-Updater',
+                                'Authorization' => 'token ' . $github_token,
+                            ]
+                        ]);
 
-                    if (is_wp_error($response)) {
-                        return $transient;
+                        if (is_wp_error($response)) {
+                            return $transient;
+                        }
+
+                        $release = json_decode(wp_remote_retrieve_body($response));
+                        if (isset($release->message) && strpos($release->message, 'API rate limit exceeded') !== false) {
+                            set_transient($cache_key, 'RATE_LIMIT', 1 * MINUTE_IN_SECONDS);
+                            return $transient;
+                        }
+                        set_transient($cache_key, $release, MINUTE_IN_SECONDS);
                     }
-
-                    $release = json_decode(wp_remote_retrieve_body($response));
-
                     if (!isset($release->tag_name)) {
                         return $transient;
                     }
@@ -85,7 +95,7 @@ if (!function_exists("github_updater_plugin_wordpress_v1")) {
                 }, 10, 2);
 
                 // Forzar actualización de plugins
-                delete_site_transient('update_plugins');
+                // delete_site_transient('update_plugins');
             }
         }
     }
